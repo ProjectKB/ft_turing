f8559c8069535d5cad1ba39e0f3f398e34354da4
Loïc Bertin <bertin.loic01@gmail.com>
2022-12-31 12:06:20 +0100

    add pseudo_universal

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse as a
 from src.parser import parse
 from src.machine import get_transitions, get_tape, show, process
 
+unary_sub_input_for_pseudo_universal = "{(r1:r1>)(r+:r1>)(r=:e.<)(e1:H.>)|r}11+111="
+
 if __name__ == '__main__':
     argparse = a.ArgumentParser()
 
